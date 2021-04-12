9b56cc359c824c56910e7bb0bcab18d73896f7ac
Kirill Sizov <sizw.kirill2013@yandex.ru>
2021-04-12 16:36:54 +0300

    addition and subtraction added

---
+++ test_a.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число а: '))
+b = int(input('Введите число b: '))
+
+def sum1(a,b):
+    return a+b
+
+print(sum1(a,b))
---
+++ test_b.py
@@ -0,0 +1,7 @@
+a = int(input('Введите число а: '))
+b = int(input('Введите число b: '))
+
+def min1(a,b):
+    return a-b
+
+print(min1(a,b))
